26ac6905045b372b0048c2f3c9bc7e839ad30e71
Anirudhan J Rajagopalan <ajr619@nyu.edu>
2015-09-28 10:52:16 -0700

    changes for hw1

--- hw1/perceptron.py
+++ hw1/perceptron.py
@@ -4,6 +4,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from sklearn import datasets
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+from sklearn.cross_validation import train_test_split
 
 scaler = StandardScaler()
 
@@ -18,15 +19,32 @@ print tmp.shape
 final_data = tmp[np.where(tmp[:,4] != 2)]
 
 X = final_data[:, [0,1, 2, 3]]
-Y = final_data[:, 4]
+y = final_data[:, 4]
 
 print("mean : %s " % X.mean(axis=0))
 print("standard deviation : %s " % X.std(axis=0))
 
 scaler.fit(X)
 
-X_scaled = scaler.transform(X)
-print(X_scaled.shape)
+X = scaler.transform(X)
+print(X.shape)
 
-print("mean : %s " % X_scaled.mean(axis=0))
-print("standard deviation : %s " % X_scaled.std(axis=0))
+print("mean : %s " % X.mean(axis=0))
+print("standard deviation : %s " % X.std(axis=0))
+
+#seed for consistency
+rng = np.random.RandomState(7041989)
+
+#print(y)
+
+permutation = rng.permutation(len(X))
+X, y = X[permutation], y[permutation]
+print(y)
+
+train_X, test_X, train_y, test_y = train_test_split(X, y, train_size=0.5, random_state=1999)
+
+print("\n\n")
+print(train_y)
+print("\n\n")
+print(test_y)
+
