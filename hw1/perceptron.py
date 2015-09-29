cb05bbd124f6ed7639e0df12d73a9aacef80a765
Anirudhan J Rajagopalan <ajr619@nyu.edu>
2015-09-29 08:14:35 -0700

    comments for the questions asked in hw.  Minor changes for making things easily configurable

--- hw1/perceptron.py
+++ hw1/perceptron.py
@@ -9,7 +9,7 @@ from sklearn.cross_validation import train_test_split
 
 MAX_ITER = 10000
 
-def batch_perceptron(X_in, y_in, max_iterations, display_format, w, label):
+def batch_perceptron(X_in, y_in, max_iterations, w, label):
     #w = np.zeros(X_in.shape[1])
     errors = []
     for t in range(0,max_iterations):
@@ -21,6 +21,7 @@ def batch_perceptron(X_in, y_in, max_iterations, display_format, w, label):
                 mis_classification += 1
         print "iteration ", t, " error ",mis_classification 
         errors.append(mis_classification)
+        # It is safe to exit the iteration once mis_classification error is zero.  Because we have already went through all the 'm' data points available
         if(mis_classification == 0):
             break
     plt.plot(errors, 'r-');
@@ -30,7 +31,7 @@ def batch_perceptron(X_in, y_in, max_iterations, display_format, w, label):
     plt.show()
     return w, errors
 
-def modified_batch_perceptron(X_in, y_in, eta, max_iterations, display_format, w, label):
+def modified_batch_perceptron(X_in, y_in, eta, max_iterations, w, label):
     #w = np.zeros(X_in.shape[1])
     errors = []
     for t in range(0,max_iterations):
@@ -87,33 +88,44 @@ train_X, test_X, train_y, test_y = load_iris_data(0, 1, 2) #takes time to conver
 #train_X, test_X, train_y, test_y = load_iris_data(1, 2, 0) # converges very fast. This one too
 
 
+#plot the dataset.  3D for better visualization.  the 4th dimension is still missing
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
+#plot features 0, 1, 2
+features = [0, 1, 2]
 colors = ["darkblue", "darkgreen"]
 idx = np.where(train_y == -1)
-ax.scatter(train_X[idx, 0], train_X[idx, 1], train_X[idx, 2], color=colors[0], label="Class %s -1")
+ax.scatter(train_X[idx, features[0]], train_X[idx, features[1]], train_X[idx, features[2]], color=colors[0], label="Class %s -1")
 idx = np.where(train_y == 1)
-ax.scatter(train_X[idx, 0], train_X[idx, 1], train_X[idx, 2], color=colors[1], label="Class %s 1")
-
+ax.scatter(train_X[idx, features[0]], train_X[idx, features[1]], train_X[idx, features[2]], color=colors[1], label="Class %s 1")
 plt.show()
+# End of training set plot
 
-w1, errors = batch_perceptron(train_X, train_y, MAX_ITER, 'ro', np.zeros(train_X.shape[1]), 'Training data')
+w1, errors = batch_perceptron(train_X, train_y, MAX_ITER, np.zeros(train_X.shape[1]), 'Training data')
 # Ideally you would want to test your learning parameter in your test data.  Here we are just running the algorithm against the test data and we might end up modifying w too.
-w2, n_errors = batch_perceptron(test_X, test_y, MAX_ITER, 'go', w1, 'Test data')
+#when you pass the w obtained before to the batch_perceptron algorithm, the algorithm converges really fast as we have already found a plane seperating the data points
+#w2, n_errors = batch_perceptron(test_X, test_y, MAX_ITER, 'go', w1, 'Test data')
+w2, n_errors = batch_perceptron(test_X, test_y, MAX_ITER, np.zeros(test_X.shape[1]), 'Test data')
 
 plt.plot(errors, 'r-');
 plt.plot(n_errors, 'b-');
 plt.title("Combined plot of train and test")
 plt.show()
-
 print "W1 is ", w1, " w2 is ", w2
+
+##############################################################
+# Observations:
+#   The perceptron algorithm may (or) may not converge depending on the split of the data.
+#   The mis-classification error count goes down for most iterations when perceptron runs on a dataset that will converge
+#   There are observable oscillation in mis-classification error when the perceptron does not converge
+##############################################################
 
 #ETA = 1 # Same as batch_perceptron
 #ETA = 0.5 # Iterations remain unchanged.  Same as batch perceptron.  changing eta changes the magnitude and not the direction
 ETA = 1.5 # Same as batch_perceptron
-w3, m_errors = modified_batch_perceptron(train_X, train_y, ETA, MAX_ITER, 'ro', np.zeros(train_X.shape[1]), 'Training data')
-w4, n_m_errors = modified_batch_perceptron(test_X, test_y, ETA, MAX_ITER, 'go', w3, 'Test data')
+w3, m_errors = modified_batch_perceptron(train_X, train_y, ETA, MAX_ITER, np.zeros(train_X.shape[1]), 'Training data')
+w4, n_m_errors = modified_batch_perceptron(test_X, test_y, ETA, MAX_ITER, w3, 'Test data')
 
 plt.plot(m_errors, 'r-');
 plt.plot(n_m_errors, 'b-');
