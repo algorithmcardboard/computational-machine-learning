ce1807e4d2171f955bd2201edc540949d0b99e35
Anirudhan J Rajagopalan <ajr619@nyu.edu>
2015-09-29 08:30:10 -0700

    adding collaborators part

--- hw1/perceptron.py
+++ hw1/perceptron.py
@@ -1,3 +1,10 @@
+##############################################################
+# Author: Anirudhan J Rajagopalan
+# email: ajr619@nyu.edu
+# Collaborators: Michele Ceru
+##############################################################
+##############################################################
+
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
